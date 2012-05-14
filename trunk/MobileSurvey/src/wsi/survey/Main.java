a065ee111bb7980cdec21ba13b024e571ef5c04d
yanggang2050@gmail.com <yanggang2050@gmail.com@2343a5c3-f9cd-a80c-39f2-ee166e36448e>
2012-05-14 04:27:59 +0000

    add MobileSurvey_v1.1.0

--- trunk/MobileSurvey/src/wsi/survey/Main.java
+++ trunk/MobileSurvey/src/wsi/survey/Main.java
@@ -1,63 +1,192 @@
 package wsi.survey;
 
-import java.io.File;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import wsi.survey.media.AudioProcess;
+import wsi.survey.result.GConstant;
+import wsi.survey.util.ImageAdapter;
+import wsi.survey.util.ImageGallery;
+
 import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import android.app.ListActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+public class Main extends Activity {
+	private final String TAG = "Main";
+	
+	private ImageGallery imgGallery;
+	private TextView tvTitle;
+	private TextView tvDescrp;
+	private ImageView ivWelcome;
+	private RelativeLayout layout_main;
+	
+	private TelephonyManager mTelManager;
 
-public class Main extends ListActivity {
+	private List<Map<String, Object>> imgList = new ArrayList<Map<String, Object>>();	// 问卷的图片资源
+	private List<Map<String, String>> mList = new ArrayList<Map<String, String>>();		// 问卷的文件名、标题、描述
 
-	public static final String filename = "FILE_NAME";
-
-	private SimpleAdapter mAdapter;
-	private List<Map<String, String>> mData = new ArrayList<Map<String, String>>();
-	private final String[] from = { "title" };
-	private int[] to;
-
+	private static boolean isFirstLoad = true;		// 是否是第一次加载（第一次加载，开机动画播放完后，开始播放背景音乐）
+	private static boolean isStartActivity = false;		// 是否是开启新Activity（是开启新Activity，true不暂停背景音乐；不是开启新Activity，false暂停背景音乐，如按Home键）
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		Log.i(TAG, "onCreate()");
 		
-		to = new int[] { android.R.id.text1 };
-		getQuestionNaireList();
-		mAdapter = new SimpleAdapter(this, mData, android.R.layout.simple_list_item_1, from, to);
-		setListAdapter(mAdapter);
+		isFirstLoad = true;
+		AudioProcess.loadMedia(this);
+		
+		initResource();
+		animPlay();
+	}
+
+	/** 初始化控件 */
+	private void initResource(){
+		ivWelcome = (ImageView)findViewById(R.id.ivWelcome);
+		imgGallery = (ImageGallery)findViewById(R.id.imgGallery);
+		tvTitle = (TextView)findViewById(R.id.tvCaption);
+		tvDescrp = (TextView)findViewById(R.id.tvDescrp);		 
+		layout_main = (RelativeLayout)findViewById(R.id.layout_main);
+		
+		mTelManager = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+		GConstant.IMEI = mTelManager.getDeviceId();
+		
+		// 获取物理屏幕
+		DisplayMetrics dm = new DisplayMetrics();
+		dm = getResources().getDisplayMetrics();
+		int screenWidth = dm.widthPixels;
+		int screenHeight = dm.heightPixels;
+		GConstant.adjustFontSize(screenWidth, screenHeight);
+		
+		Log.i(TAG, "dm.density = " + dm.density + "; screenWidth = " + screenWidth + "; screenHeight = " + screenHeight);
+	}
+	
+	/** 播放开机动画 */
+	private void animPlay(){
+		Animation anim = AnimationUtils.loadAnimation(Main.this, R.anim.anim_main_welcome);
+		anim.setFillEnabled(true); 
+		anim.setFillAfter(true);  
+		ivWelcome.startAnimation(anim);
+		
+		Message msg = Message.obtain();
+		mHandler.sendMessageDelayed(msg, 3000);
+	}
+	
+	private Handler mHandler = new Handler(){
+		@Override
+		public void handleMessage(Message msg){
+			ivWelcome.setVisibility(View.INVISIBLE);
+			layout_main.setBackgroundResource(R.drawable.bg_main);
+			loadResource();			// 开机动画后，开始加载资源并显示
+		}
+	};
+	
+	/** 加载资源，为控件赋值 */
+	private void loadResource(){
+		AudioProcess.playMedia();
+		isFirstLoad = false;
+		
+		for(int i=0; i<GConstant.surveyFiles.length; i++){
+			Map<String, Object> imgMap = new HashMap<String, Object>();	// 图片资源
+			imgMap.put("images", GConstant.imgs[i]);
+			imgList.add(imgMap);
+			
+			Map<String, String> map = new HashMap<String, String>();		// 文件名、标题、描述
+			map.put("fileName", GConstant.surveyFiles[i][0]);
+			map.put("fileTitle", GConstant.surveyFiles[i][1]);
+			map.put("fileDescrp", GConstant.surveyFiles[i][2]);
+			mList.add(map);
+		}
+		
+		ImageAdapter imgAdapter = new ImageAdapter(this, imgList);
+		imgAdapter.createReflectedImages();
+		imgGallery.setAdapter(imgAdapter);
+		
+		imgGallery.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				tvTitle.setTextSize(GConstant.titleFontSize);
+				tvTitle.setText(GConstant.surveyFiles[position][1]);
+				tvDescrp.setText(GConstant.surveyFiles[position][2]);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
+		
+		imgGallery.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent intent = new Intent(Main.this, AnswerQuesion.class);
+				intent.putExtra("fileName", GConstant.surveyFiles[position][0]);
+				isStartActivity = true;
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
-	protected void onListItemClick(ListView list, View view, int position, long id) {
-		super.onListItemClick(list,  view,  position,  id);
+	protected  void onStart(){
+		super.onStart();
+		Log.i(TAG, "onStart()");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.i(TAG, "onResume()");
+		isStartActivity = false;
 		
-		Intent intent = new Intent(this, AnswerQuesionNaire.class);
-		Bundle bundle = new Bundle();
-		Map<String, String> map = mData.get(position);
-
-		bundle.putString(filename, map.get(from[0]));
-		intent.putExtras(bundle);
-		startActivity(intent);
-	}
-
-	public void getQuestionNaireList() {
-		String filePath = Environment.getExternalStorageDirectory() + "/CompterThink/";
-		File file = new File(filePath);
-		File files[] = file.listFiles();
-		int len = files.length;
-
-		for (int i = 0; i < len; i++) {
-			Hashtable<String, String> table = new Hashtable<String, String>();
-			table.put(from[0], files[i].getName());
-			mData.add(table);
+		if(!isFirstLoad){
+			AudioProcess.playMedia();
+			Log.i(TAG, "isFirstLoad = " + isFirstLoad);
+		} else {
+			Log.i(TAG, "isFirstLoad = " + isFirstLoad);
 		}
-
+		
 	}
 
+	@Override
+	protected  void onPause() {
+		super.onPause();
+		Log.i(TAG, "onPause()");
+		if(!isStartActivity){
+			AudioProcess.pauseMedia();
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.i(TAG, "onStop()");
+	}
+	
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		Log.i(TAG, "onDestroy()");
+		
+		AudioProcess.stopMedia();
+		AudioProcess.stopSound();
+	}
 }
--- trunk/MobileSurvey/src/wsi/survey/question/QuestionItem.java
+++ trunk/MobileSurvey/src/wsi/survey/question/QuestionItem.java
@@ -1,31 +1,47 @@
 package wsi.survey.question;
 
+
 public class QuestionItem {
-	private int id;
-	private String title;
+	private String qid;			// 问题编号number，如 17道问题，即 1~17
+	private String tag;
+	private String questionType;
+	private String title;		// 问题题目content
+	private OptionItem []optionItems;
+	private int oidx;
+	private int oidx_selected;
 	private int optionNum;
-	private String option[];
-	private int idx = 0;
 
-	private int answer = 0;
-
-	private boolean[] answerMoption;
+	private boolean[] answerMoption;	// 用于多选项的 checked
 
 	public QuestionItem() {
-		this.option = new String[6];
-		this.answerMoption = new boolean[6];
-		for (int i = 0; i < 6; i++) {
-			answerMoption[i] = false;
-		}
+		this.oidx = 0;
+		this.oidx_selected = -1;
 	}
 
-	public QuestionItem(int id, String title, int optionNum) {
-		this.id = id;
-		this.title = title;
-		this.optionNum = optionNum;
-		this.option = new String[optionNum];
+	public void setQid(String qid) {
+		this.qid = qid;
 	}
 
+	public String getQid(){
+		return this.qid;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getTag(){
+		return this.tag;
+	}
+
+	public void setQuestionType(String questionType) {
+		this.questionType = questionType;
+	}
+
+	public String getQuestionType(){
+		return this.questionType;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
@@ -34,37 +50,52 @@ public class QuestionItem {
 		this.title = title;
 	}
 
-	public void setId(String id) {
-		this.id = Integer.parseInt(id);
+	public void setOptionsNum(String questionType, int optionNum) {
+		this.optionItems = new OptionItem[optionNum];	
+		
+		if(questionType.equals("multi")){	// checked 多选
+			this.answerMoption = new boolean[optionNum];
+			for (int i = 0; i < optionNum; i++) {
+				answerMoption[i] = false;
+			}
+		}
 	}
 
-	public void setOption(String optionItem) {
-		this.option[idx] = optionItem;
-		idx++;
+	public void setOptionItem(int oidx, OptionItem optionItem) {
+		this.optionItems[oidx] = optionItem;
+		this.oidx = oidx;
 	}
 
-	public int getOptionNum() {
-		return idx;
+	public OptionItem getOptionItem(int oidx) {
+		if(oidx >= 0) {
+			return optionItems[oidx];
+		}
+		return null;
 	}
 
-	public String getOptionItem(int idx) {
-		return option[idx];
+	public int getOptionItemIndex() {
+		return oidx;
 	}
 
-	public void setAnswer(int idx) {
-		this.answer = idx;
+	public int getOptionItemNum() {
+		this.optionNum = Math.max(0, oidx+1);
+		return this.optionNum;
 	}
 
-	public int getAnswer() {
-		return this.answer;
+	public void setAnswerOptionIndex(int oidx_selected) {
+		this.oidx_selected = oidx_selected;
+	}
+	
+	public int getAnswerOptionIndex() {
+		return this.oidx_selected;
 	}
 
-	public void setAnswerOfM(boolean answer, int idx) {
-		this.answerMoption[idx] = answer;
+	public void setAnswerOfM(boolean answer, int oidx) {
+		this.answerMoption[oidx] = answer;
 	}
 
-	public boolean getAnswerofM(int idx) {
-		return this.answerMoption[idx];
+	public boolean getAnswerofM(int oidx) {
+		return this.answerMoption[oidx];
 	}
 
 }
